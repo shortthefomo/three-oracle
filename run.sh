364f7fba31a4cf81a2528d35aad37bac31d0da24
Lathan Britz <jucallme@gmail.com>
2024-11-20 20:32:05 -0400

    dev

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export NODE_ENV=production
 export DEBUG=apps*
 export DEBUG_COLORS=true
-pm2 start "yarn start" ./src/index.js --name three-oracle --time --max-memory-restart 4096
+pm2 start "yarn dev" ./src/index.js --name three-oracle --time
