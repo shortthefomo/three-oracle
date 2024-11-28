23112d6d344a4553eb3d1877e407c949afcb78ff
Lathan Britz <jucallme@gmail.com>
2024-11-27 20:52:12 -0400

    track mem

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 export NODE_ENV=production
 export DEBUG=apps*
 export DEBUG_COLORS=true
-pm2 start ./src/main.js --max_memory_restart 1000M --name three-oracle
+pm2 start ./src/main.js --name three-oracle --max_memory_restart 1000M
