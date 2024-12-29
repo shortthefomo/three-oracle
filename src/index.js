2fc2e165df7bc0347d5eb9358a74c69d0f78696a
Lathan Britz <jucallme@gmail.com>
2024-12-29 18:11:45 -0400

    this needs to be updated now

--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ class service extends EventEmitter {
 				this.addListener('memstats', async () => {
 					this.logAppStats()
 					log('data size', Object.keys(data_copy).length)
-					if (Object.keys(data_copy).length <= 1) {
+					if (Object.keys(data_copy).length <= 2) {
 						connected = false
 						log('reconnect no data ---------------->')
 						this.emit('kill-process')
