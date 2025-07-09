c0012e027ffcc68396a11b5f000acd9574e6ae76
Lathan <jucallme@gmail.com>
2025-07-09 16:09:54 -0400

    up limit

--- src/index.js
+++ src/index.js
@@ -85,7 +85,7 @@ class service extends EventEmitter {
 				this.addListener('memstats', async () => {
 					this.logAppStats()
 					log('data size', Object.keys(data_copy).length)
-					if (Object.keys(data_copy).length <= 5) {
+					if (Object.keys(data_copy).length <= 6) {
 						connected = false
 						log('reconnect no data ---------------->')
 						this.emit('kill-process')
